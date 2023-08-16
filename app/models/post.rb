3f167ec97f8ba167bdb0f1207dcfa26e9c3d9a8b
unknown <teddytiyi@gmail.com>
2023-08-17 00:31:52 +0200

    add moduels

---
+++ app/models/post.rb
@@ -0,0 +1,14 @@
+class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+  
+    def update_author_posts_counter
+      author.update(posts_counter: author.posts.count)
+    end
+  
+    def recent_comments(limit = 5)
+      comments.order(created_at: :desc).limit(limit)
+    end
+  end
+  
