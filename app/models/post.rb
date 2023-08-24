d960a6ee9d29f14444da831eb39603af218a4463
unknown <teddytiyi@gmail.com>
2023-08-24 18:46:49 +0200

    Add Views

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,12 +5,16 @@ class Post < ApplicationRecord
 
   after_create :update_posts_counter
 
-  validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  # validates :title, presence: true, length: { maximum: 250 }
+  # validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
+  # validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def recent_five_comments
     comments.includes([:user]).order(created_at: :desc).limit(5)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc)
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: 'author_id', dependent: :destroy
-  has_many :comments
-  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 
-  validates :name, presence: true
+  # validates :name, presence: true
 
   def recent_posts(limit = 3)
     posts.order(created_at: :desc).limit(limit)
+  end
+
+  def posts_counter
+    posts.count
   end
 end
