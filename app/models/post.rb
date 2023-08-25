b987eb4a042d1f8ae87d45b4074021fff9bb4d4f
unknown <teddytiyi@gmail.com>
2023-08-25 02:53:22 +0200

    modified db

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy, foreign_key: :post_id
   has_many :likes, dependent: :destroy, foreign_key: :post_id
 
   after_create :update_posts_counter
 
   def recent_five_comments
-    comments.includes([:user]).order(created_at: :desc).limit(5)
+    comments.includes([:author]).order(created_at: :desc).limit(5)
   end
 
   def recent_comments
@@ -16,6 +16,6 @@ class Post < ApplicationRecord
   private
 
   def update_posts_counter
-    user.increment!(:posts_counter)
+    author.increment!(:posts_counter)
   end
 end
