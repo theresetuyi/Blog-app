f7cef6a2956c82513d6808ded9837b16a330b54c
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-08-28 15:25:50 -0600

    Solve the N+1 problem for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = Post.where(author_id: params[:user_id])
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments).find(params[:id])
   end
 
   def new
